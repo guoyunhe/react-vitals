7f5efbbcf9ae259cc3de0128dc46b85c326485c0
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-12 14:03:14 +0800

    start end

--- src/SlowSkip.tsx
+++ src/SlowSkip.tsx
@@ -1,17 +1,26 @@
 import React, { ReactNode, useEffect, useMemo, useState } from 'react';
 
 export interface SlowSkipProps {
-  timeout?: number;
+  /**
+   * Milliseconds related to navigation start.
+   * If the component is mounted later than this time, content will **NOT** be rendered.
+   */
+  start?: number;
+  /**
+   * Milliseconds related to navigation start.
+   * If the component is mounted later than this time, content will be rendered.
+   */
+  end?: number;
   children?: ReactNode;
 }
 
-export function SlowSkip({ children, timeout = 1500 }: SlowSkipProps) {
+export function SlowSkip({ children, start = 1500, end = 10000 }: SlowSkipProps) {
   const [show, setShow] = useState(false);
   const now = useMemo(() => performance.now(), []);
 
   useEffect(() => {
-    setShow(now < timeout || now > 60000);
-  }, [timeout, now]);
+    setShow(now < start || now > end);
+  }, [start, now]);
 
   if (show) {
     return <>{children}</>;
